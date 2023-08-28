df1a83c1b22224ce82312fda6b295bc4da14e1ce
duanjp8617 <duanjp8617@gmail.com>
2023-08-28 11:30:40 +0800

    update settings

--- tvm/post_build_setup.sh
+++ tvm/post_build_setup.sh
@@ -10,7 +10,7 @@ if [ -d "/workspace/tvm" ]; then
   echo "tips:
 1. Run this command in build directory: cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ..
    It generates the build/compile_commands.json, which is used by clangd to index the source files.
-2. clang-tidy is enabled by copying the /workspace/configs/config.yaml to ~/.config/clangd/config.yaml"
+2. clang-tidy is enabled by copying the /workspace/configs/config.yaml to $HOME/.config/clangd/config.yaml"
 else 
   echo "/workspace/tvm is not available, have you map tvm source code to the container?"
 fi
