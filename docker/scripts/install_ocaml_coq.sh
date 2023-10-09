9a496249870c13462ea7e1a5f85f8d8d72cca459
duanjp8617 <duanjp8617@gmail.com>
2023-10-09 13:13:06 +0800

    update ocaml_coq dockerfile

--- docker/scripts/install_ocaml_coq.sh
+++ docker/scripts/install_ocaml_coq.sh
@@ -13,6 +13,7 @@ install -m 755 ./$OPAM_BIN /usr/local/bin/opam
 
 opam init --disable-sandboxing --dot-profile=~/.bashrc --shell-setup
 opam switch create 4.14.0
-opam install -y --assume-depexts menhir dune merlin ocaml-lsp-server odoc ocamlformat utop dune-release tuareg
-opam pin add -y coq 8.16.1
+opam install -y dune merlin ocaml-lsp-server odoc ocamlformat utop dune-release menhir
+opam pin add -y coq 8.17.1
+opam install -y coq-lsp
 
