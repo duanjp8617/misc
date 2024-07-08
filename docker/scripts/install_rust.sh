ec12a8a6c88ed70411c8c2cd71b9abb5244eb7df
duanjp8617 <duanjp8617@gmail.com>
2024-07-08 17:47:39 +0800

    sync

--- docker/scripts/install_rust.sh
+++ docker/scripts/install_rust.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-apt update
-
-apt install -y curl
+# apt update
+# apt install -y curl
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
 
-rustup component add rust-src
-rustup component add rust-analyzer
+#rustup component add rust-src
+#rustup component add rust-analyzer
